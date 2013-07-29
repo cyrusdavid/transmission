dc17716c21df95f17ebb6060a79c4ab0432a9fa0
vohof <david@jcyr.us>
2013-07-29 20:03:16 +0800

    must be compatible with parent class

--- src/Vohof/GuzzleException.php
+++ src/Vohof/GuzzleException.php
@@ -2,9 +2,14 @@
 
 class GuzzleException extends \Guzzle\Http\Exception\BadResponseException {
 
-    public static function factory($message, RequestInterface $request, Response $response)
+    public static function forge($message)
     {
-        $e = new $class($message);
+        $this->message = $message;
+    }
+
+    public static function factory(RequestInterface $request, Response $response)
+    {
+        $e = new $class($this->message);
         $e->setResponse($response);
         $e->setRequest($request);
 
